f88ec272d2e9a62ed4256062a087ea23c082981e
Kamaluddin <97792683+SayedKamaluddin@users.noreply.github.com>
2025-05-01 14:17:03 -0600

    made the chracter alive and its now moving when i call it to be in idle from the chracter class and its also able to blink and working

--- sketch.js
+++ sketch.js
@@ -11,33 +11,64 @@ class Character{
     this.speed = speed;
     this.strenght = strenght;
     this.health = health;
+    // this.idle = img[0];
+    // this.blink = img[1];
+    // this.walk = img[2];
+    // this.slash = img[3];
+    // this.throw = img[4];
+    // this.die = img[5];
     this.img = img;
     this.x = x;
     this.y = y;
+    this.frame = 1;
   }
 
-  display(){
-    image(this.img,this.x,this.y, 1000,100);
+  idle(){
+    // print(this.frame, this.img.height*floor(this.frame));
+    image(this.img[0], 200, 100, 100, 100, this.img[0].height*floor(this.frame), 0, this.img[0].height);
+    if (this.frame*this.img[0].height > this.img[0].width){
+      this.frame = 1;
+    }
+    else{
+      this.frame+=0.2;
+    }
   }
 
-  move(){
+  blink(){
+    image(this.img[1], 200, 100, 100, 100, this.img[1].height*floor(this.frame), 0, this.img[1].height);
+    if (this.frame*this.img[1].height > this.img[1].width){
+      this.frame = 1;
+    }
+    else{
+      this.frame+=0.2;
+    }
     this.x += this.speed;
   }
 }
 
 
-let filesToPreload = ['glm'];
-let bolderImg, path;
+let filesToPreload = ['idle','blink','walk','slash','die'];
+let imgHeight = 100;
 
-
+//difine all characters
 let bolder;
+let bolderImg = [];
 
+// function helpPreload(folderName,fileNUmber){
+//   bolderImg.push(loadImage('characters\\'+folderName+'\\'+fileNUmber+'\\idle.jpeg'));
+//   bolderImg.push(loadImage('characters\\'+folderName+'\\'+fileNUmber+'\\blink.jpeg'));
+//   bolderImg.push(loadImage('characters\\'+folderName+'\\'+fileNUmber+'\\walk.jpeg'));
+//   bolderImg.push(loadImage('characters\\'+folderName+'\\'+fileNUmber+'\\slash.jpeg'));
+//   bolderImg.push(loadImage('characters\\'+folderName+'\\'+fileNUmber+'\\throw.jpeg'));
+//   bolderImg.push(loadImage('characters\\'+folderName+'\\'+fileNUmber+'\\die.jpeg'));
+  
+// }
 
 function preload(){
-  for(let names of filesToPreload){
-    path = 'characters\\bolder\\blo1\\'+names+'_idle.jpeg';
-    bolderImg = loadImage(path);
-  } 
+  for(let file of filesToPreload){
+    bolderImg.push(loadImage('characters\\bolder\\1\\'+file+'.jpeg'));
+  }
+  
 }
 
 function runInSetup(){
@@ -49,6 +80,9 @@ function setup() {
   runInSetup();
 }
 
+// let frame = 1;
 function draw() {
-  bolder.display();
+  bolder.blink();
+
+  
 }
