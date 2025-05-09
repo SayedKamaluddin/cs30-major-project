72ea9e9e71e95f8f965f802b055ef88135c83fd2
Kamaluddin <97792683+SayedKamaluddin@users.noreply.github.com>
2025-05-09 14:17:03 -0600

    created the character holder on the top left where the characters are at rest untill they're picked also worked making an enemy character start attacking

--- sketch.js
+++ sketch.js
@@ -7,21 +7,16 @@
 
 
 class Character{
-  constructor(name, speed, strenght, health, img, x, y){
+  constructor(name, speed, strenght, health, img, x, y, diraction){
     this.name = name;
     this.speed = speed;
     this.strenght = strenght;
     this.health = health;
-    // this.idle = img[0];
-    // this.blink = img[1];
-    // this.walk = img[2];
-    // this.slash = img[3];
-    // this.throw = img[4];
-    // this.die = img[5];
     this.img = img;
     this.x = x;
     this.y = y;
     this.frame = 1;
+    this.diraction = diraction;
   }
 
   showHealth(){
@@ -48,7 +43,12 @@ class Character{
   
   walk(){
     this.action(2);
-    this.x += this.speed;
+    if (this.diraction === 'r'){
+      this.x += this.speed;
+    }
+    else if (this.diraction === 'l'){
+      this.x -= this.speed;
+    }
   }
   
   slash(){
@@ -61,6 +61,10 @@ class Character{
 
   die(){
     this.action(5);
+  }
+
+  alive(){
+
   }
 }
 
@@ -103,7 +107,9 @@ function preload(){
   for (let map in maps){
     maps[map] = loadImage('maps\\'+map+'.png');
   }
-  print(maps.greenland);
+  topleft = loadImage('maps\\leftcharplace.png');
+  topright = loadImage('maps\\rightcharplace.png');
+
 }
 
 
@@ -118,6 +124,11 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
   runInSetup();
+  
+}
+
+
+function eneamyBolder(){
   
 }
 
